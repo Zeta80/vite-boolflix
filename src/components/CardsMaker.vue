c42f665649b50e6cbcd6e7be72c0c28a9970884b
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-14 16:26:48 +0100

    add flag text

--- src/components/CardsMaker.vue
+++ src/components/CardsMaker.vue
@@ -8,11 +8,17 @@ export default {
 </script>
 
 <template>
-    <div v-if="result.original_title" class="character-item text-center">
-        <h4>{{ result.original_title }}</h4>
-    </div>
-    <div v-else>
-        <h4>{{ result.name }}</h4>
+    <div class="wrapper">
+
+        <div v-if="result.original_title" class="character-item text-center">
+            <h4>{{ result.original_title }}</h4>
+        </div>
+        <div v-else>
+            <h4>{{ result.name }}</h4>
+        </div>
+        <div class="flag">
+            <h3> {{ result.original_language }} </h3>
+        </div>
     </div>
 </template>
 
