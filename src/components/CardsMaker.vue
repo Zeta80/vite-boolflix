4efb24d360ecc99e6a9e7a494ad77aa72971b5ee
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-15 13:42:57 +0100

    first version star

--- src/components/CardsMaker.vue
+++ src/components/CardsMaker.vue
@@ -7,40 +7,72 @@ export default {
     },
     data() {
         return {
-            flag: "../assets/immagini-flag/italia.png",
+            availableFlags: ['en', 'it'],
             store,
+
 
         }
     },
+    computed: {
+        stars() {
+            const starsNumber = (Number(this.result.vote_average) / 2).toFixed(0)
+            return starsNumber
+        }
+    },
     methods: {
-        flagConvertor(flag) {
+        flagConvertor() {
             if (this.result.original_language == "it") {
                 return "../assets/immagini-flag/italia.png"
             }
-        }
+        },
+        getImgUrl(imgName) {
+            return new URL(`../assets/immagini-flag/${imgName}.png`, import.meta.url).href;
+        },
+
     },
 }
 </script>
 
 <template>
-    <div class="container wrapper">
-        <div class="img-card">
-            <img :src="`${store.imgCard}${result.poster_path} `">
-        </div>
-        <div v-if="result.original_title" class="card-item text-center">
-            <h4>{{ result.original_title }}</h4>
-        </div>
-        <div v-else>
-            <h4>{{ result.name }}</h4>
-        </div>
-        <div class="flag">
-            <img :src="flagConvertor(flag)" alt="">
+    <div class="container ">
+        <div class="wrapper">
+            <div class="img-card">
 
+                <img :src="`${store.imgCard}${result.poster_path} `">
+            </div>
+
+            <div v-if="result.original_title" class="card-item text-center">
+                <h3>Titolo film:{{ result.title }}</h3>
+                <h4>Titolo orignale:{{ result.original_title }}</h4>
+            </div>
+            <div v-else>
+                <h3>Titolo Serie:{{ result.name }}</h3>
+                <h4>Titolo orignale:{{ result.original_name }}</h4>
+            </div>
+            <div class="flag">
+                <img v-if="availableFlags.includes(result.original_language)"
+                    :src="getImgUrl(result.original_language)">
+                <p v-else> Lenguage: {{ result.original_language }} </p>
+            </div>
+            <div class="star">
+                <p>{{ result.vote_average }}</p>
+                <span v-for="item in 5">
+                    {{ stars }}
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.wrapper {
+    text-align: start;
+}
+
+.img-card {
+    position: relative;
+}
+
 .card-item {
     padding: 15px;
     height: 100%;
